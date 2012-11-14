45d5db201ba75b5810e448fb4592d9e342b4708d
Kyle Burckhard <talaris@gmail.com>
2012-11-14 10:57:40 -0800

    Fix commas in json

--- solr-memory.js
+++ solr-memory.js
@@ -1,152 +1,152 @@
 var metrics = [
   {
     "alias": 'solrcloud01',
-    "target": "stats.solrcloud01.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud01.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
     {
     "alias": 'solrcloud02',
-    "target": "stats.solrcloud02.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud02.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud03',
-    "target": "stats.solrcloud03.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud03.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud04',
-    "target": "stats.solrcloud04.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud04.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud05',
-    "target": "stats.solrcloud05.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud05.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud06',
-    "target": "stats.solrcloud06.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud06.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud07',
-    "target": "stats.solrcloud07.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud07.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud08',
-    "target": "stats.solrcloud08.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud08.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud09',
-    "target": "stats.solrcloud09.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud09.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud10',
-    "target": "stats.solrcloud10.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud10.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud11',
-    "target": "stats.solrcloud11.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud11.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud12',
-    "target": "stats.solrcloud12.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud12.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud13',
-    "target": "stats.solrcloud13.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud13.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud14',
-    "target": "stats.solrcloud14.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud14.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud15',
-    "target": "stats.solrcloud15.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud15.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud16',
-    "target": "stats.solrcloud16.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud16.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud17',
-    "target": "stats.solrcloud17.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud17.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud18',
-    "target": "stats.solrcloud18.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud18.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud19',
-    "target": "stats.solrcloud19.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud19.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud20',
-    "target": "stats.solrcloud20.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud20.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud21',
-    "target": "stats.solrcloud21.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud21.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud22',
-    "target": "stats.solrcloud22.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud22.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud23',
-    "target": "stats.solrcloud23.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud23.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud24',
-    "target": "stats.solrcloud24.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud24.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud25',
-    "target": "stats.solrcloud25.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud25.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud26',
-    "target": "stats.solrcloud26.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud26.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud27',
-    "target": "stats.solrcloud27.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud27.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud28',
-    "target": "stats.solrcloud28.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud28.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud29',
-    "target": "stats.solrcloud29.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud29.memory.freeWOBuffersCaches",
+    "unit": "MB"
   },
   {
     "alias": 'solrcloud30',
-    "target": "stats.solrcloud30.memory.freeWOBuffersCaches"
-    "unit": "MB",
+    "target": "stats.solrcloud30.memory.freeWOBuffersCaches",
+    "unit": "MB"
   }
 ];
