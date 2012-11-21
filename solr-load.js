6f56525696fc1eb2f4e14bc0b9b2879f66b68cbb
Kyle Burckhard <talaris@gmail.com>
2012-11-21 00:36:45 -0800

    Use 1 minute load averages

--- solr-load.js
+++ solr-load.js
@@ -1,181 +1,181 @@
 var metrics = [
   {
     "alias": 'solrcloud01',
-    "target": "stats.solrcloud01.load_avg.five",
+    "target": "stats.solrcloud01.load_avg.one",
     "warning": 1,
     "critical": 2
   },
     {
     "alias": 'solrcloud02',
-    "target": "stats.solrcloud02.load_avg.five",
+    "target": "stats.solrcloud02.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud03',
-    "target": "stats.solrcloud03.load_avg.five",
+    "target": "stats.solrcloud03.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud04',
-    "target": "stats.solrcloud04.load_avg.five",
+    "target": "stats.solrcloud04.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud05',
-    "target": "stats.solrcloud05.load_avg.five",
+    "target": "stats.solrcloud05.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud06',
-    "target": "stats.solrcloud06.load_avg.five",
+    "target": "stats.solrcloud06.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud07',
-    "target": "stats.solrcloud07.load_avg.five",
+    "target": "stats.solrcloud07.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud08',
-    "target": "stats.solrcloud08.load_avg.five",
+    "target": "stats.solrcloud08.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud09',
-    "target": "stats.solrcloud09.load_avg.five",
+    "target": "stats.solrcloud09.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud10',
-    "target": "stats.solrcloud10.load_avg.five",
+    "target": "stats.solrcloud10.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud11',
-    "target": "stats.solrcloud11.load_avg.five",
+    "target": "stats.solrcloud11.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud12',
-    "target": "stats.solrcloud12.load_avg.five",
+    "target": "stats.solrcloud12.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud13',
-    "target": "stats.solrcloud13.load_avg.five",
+    "target": "stats.solrcloud13.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud14',
-    "target": "stats.solrcloud14.load_avg.five",
+    "target": "stats.solrcloud14.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud15',
-    "target": "stats.solrcloud15.load_avg.five",
+    "target": "stats.solrcloud15.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud16',
-    "target": "stats.solrcloud16.load_avg.five",
+    "target": "stats.solrcloud16.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud17',
-    "target": "stats.solrcloud17.load_avg.five",
+    "target": "stats.solrcloud17.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud18',
-    "target": "stats.solrcloud18.load_avg.five",
+    "target": "stats.solrcloud18.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud19',
-    "target": "stats.solrcloud19.load_avg.five",
+    "target": "stats.solrcloud19.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud20',
-    "target": "stats.solrcloud20.load_avg.five",
+    "target": "stats.solrcloud20.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud21',
-    "target": "stats.solrcloud21.load_avg.five",
+    "target": "stats.solrcloud21.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud22',
-    "target": "stats.solrcloud22.load_avg.five",
+    "target": "stats.solrcloud22.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud23',
-    "target": "stats.solrcloud23.load_avg.five",
+    "target": "stats.solrcloud23.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud24',
-    "target": "stats.solrcloud24.load_avg.five",
+    "target": "stats.solrcloud24.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud25',
-    "target": "stats.solrcloud25.load_avg.five",
+    "target": "stats.solrcloud25.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud26',
-    "target": "stats.solrcloud26.load_avg.five",
+    "target": "stats.solrcloud26.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud27',
-    "target": "stats.solrcloud27.load_avg.five",
+    "target": "stats.solrcloud27.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud28',
-    "target": "stats.solrcloud28.load_avg.five",
+    "target": "stats.solrcloud28.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud29',
-    "target": "stats.solrcloud29.load_avg.five",
+    "target": "stats.solrcloud29.load_avg.one",
     "warning": 1,
     "critical": 2
   },
   {
     "alias": 'solrcloud30',
-    "target": "stats.solrcloud30.load_avg.five",
+    "target": "stats.solrcloud30.load_avg.one",
     "warning": 1,
     "critical": 2
   }
