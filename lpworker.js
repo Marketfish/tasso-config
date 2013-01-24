9c73286ec98e521493b904ce9e89f0f09be3ced7
Kyle Burckhard <talaris@gmail.com>
2013-01-24 14:23:15 -0800

    Added listproc and reordered.

--- lpworker.js
+++ lpworker.js
@@ -1,5 +1,7 @@
 var metrics = [
+
   { "alias": "solr_load", "target": "averageSeries(stats.solrcloud*.load_avg.one)" },
+  { "alias": "listproc_load", "target": "stats.listprocessing01.load_avg.one" },
   {
     "alias": 'lpworker_total',
     "target": "sumSeries(stats.*.lpworker.records_per_second.*)"
@@ -10,26 +12,28 @@ var metrics = [
     "warning": 1,
     "critical": 2
   },
-  {
-    "alias": 'services01_rate',
-    "target": "sumSeries(stats.services01.lpworker.records_per_second.*)",
-  },
-    {
+      {
     "alias": 'workhorse01_load',
     "target": "stats.workhorse01.load_avg.one",
     "warning": 1,
     "critical": 2
   },
-  {
-    "alias": 'workhorse01_rate',
-    "target": "sumSeries(stats.workhorse01.lpworker.records_per_second.*)",
-  },
-  {
+    {
     "alias": 'workhorse02_load',
     "target": "stats.workhorse02.load_avg.one",
     "warning": 1,
     "critical": 2
   },
+  {
+    "alias": 'services01_rate',
+    "target": "sumSeries(stats.services01.lpworker.records_per_second.*)",
+  },
+
+  {
+    "alias": 'workhorse01_rate',
+    "target": "sumSeries(stats.workhorse01.lpworker.records_per_second.*)",
+  },
+
   {
     "alias": 'workhorse02_rate',
     "target": "sumSeries(stats.workhorse02.lpworker.records_per_second.*)",
